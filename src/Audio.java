c21c93b9404553f5fa1215300518673500626ea3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-28 17:12:44 -0300

    adição dos atributos de Audio

--- src/Audio.java
+++ src/Audio.java
@@ -1,2 +1,7 @@
 public class Audio {
+    private String titulo;
+    private int duracao;
+    private int totalDeReproducoes;
+    private int curtidas;
+    //private Classificavel classificacao;
 }
