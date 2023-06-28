0d866c6c25eb4c034f17b502992c174ff037aab8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-28 19:06:39 -0300

    adição dos atributos e encapsulamento da classe Podcast

--- src/com/github/lucasdevrj/harken/modelos/Podcast.java
+++ src/com/github/lucasdevrj/harken/modelos/Podcast.java
@@ -1,4 +1,22 @@
 package com.github.lucasdevrj.harken.modelos;
 
 public class Podcast extends Audio {
+    private String descricao;
+    private int totalDeEpisodios;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getTotalDeEpisodios() {
+        return totalDeEpisodios;
+    }
+
+    public void setTotalDeEpisodios(int totalDeEpisodios) {
+        this.totalDeEpisodios = totalDeEpisodios;
+    }
 }
