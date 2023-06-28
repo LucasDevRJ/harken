70420c2987ef423300b0e16f872116c4c3a4528e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-28 18:32:01 -0300

    criação dos métodos Getters e Setters do Audio

--- src/com/github/lucasdevrj/harken/modelos/Audio.java
+++ src/com/github/lucasdevrj/harken/modelos/Audio.java
@@ -7,6 +7,38 @@ public class Audio {
     private int curtidas;
     //private Classificavel classificacao;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public int getTotalDeReproducoes() {
+        return totalDeReproducoes;
+    }
+
+    public void setTotalDeReproducoes(int totalDeReproducoes) {
+        this.totalDeReproducoes = totalDeReproducoes;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
     public void curtir() {
         this.curtidas++;
     }
