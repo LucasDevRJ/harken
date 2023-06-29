5e80f569e74dfcd5e032ef26979e1c258b0632d0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-29 20:44:11 -0300

    adição dos atributos e encapsulamento do Genero

--- src/com/github/lucasdevrj/harken/modelos/Genero.java
+++ src/com/github/lucasdevrj/harken/modelos/Genero.java
@@ -1,4 +1,16 @@
 package com.github.lucasdevrj.harken.modelos;
 
+import java.util.ArrayList;
+
 public class Genero {
+    private String nome;
+    private ArrayList<Musica> listaMusicas = new ArrayList<Musica>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
