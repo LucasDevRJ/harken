e75a2bcae6abb92c4a9748e0c65bfc56f683b732
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-28 21:54:59 -0300

    método de adicionar música no album

--- src/com/github/lucasdevrj/harken/Album.java
+++ src/com/github/lucasdevrj/harken/Album.java
@@ -1,5 +1,7 @@
 package com.github.lucasdevrj.harken;
 
+import com.github.lucasdevrj.harken.modelos.Musica;
+
 public class Album {
     private String nome;
     private int numeroDeMusicas;
@@ -15,20 +17,18 @@ public class Album {
         this.nome = nome;
     }
 
+    public void adicionaMusica(Musica musica) {
+        System.out.println("Música " + musica.getTitulo() + " adicionada no album!");
+        this.numeroDeMusicas++;
+        this.duracao += musica.getDuracao();
+    }
+
     public int getNumeroDeMusicas() {
         return numeroDeMusicas;
     }
 
-    public void setNumeroDeMusicas(int numeroDeMusicas) {
-        this.numeroDeMusicas = numeroDeMusicas;
-    }
-
     public int getDuracao() {
         return duracao;
-    }
-
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
     }
 
     public String getData() {
