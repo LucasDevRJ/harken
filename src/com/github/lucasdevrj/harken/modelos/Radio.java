2bb82ac1e0633f685d353a4dcea0785323b04adb
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-30 07:54:28 -0300

    classe Radio encapsulada

--- src/com/github/lucasdevrj/harken/modelos/Radio.java
+++ src/com/github/lucasdevrj/harken/modelos/Radio.java
@@ -1,4 +1,13 @@
 package com.github.lucasdevrj.harken.modelos;
 
 public class Radio extends Audio {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
