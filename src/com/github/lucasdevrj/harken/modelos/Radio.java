5cbd64ea194285d40a5d9dd4b0f5ac9384645686
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-06-30 11:16:03 -0300

    método para adicionar radios

--- src/com/github/lucasdevrj/harken/modelos/Radio.java
+++ src/com/github/lucasdevrj/harken/modelos/Radio.java
@@ -1,6 +1,20 @@
 package com.github.lucasdevrj.harken.modelos;
 
+import java.util.ArrayList;
+
 public class Radio extends Audio {
+
+    private ArrayList<Radio> listaRadios = new ArrayList<Radio>();
+
+    public ArrayList<Radio> getListaRadios() {
+        return listaRadios;
+    }
+
+    public void adicionaRadio(Radio radio) {
+        System.out.println("A estação " + radio.titulo + " foi adicionada!!");
+        this.listaRadios.add(radio);
+    }
+
     public void mudaEstacao(Radio radio) {
         String radioDesejada = radio.titulo;
         if (!this.titulo.equals(radioDesejada)) {
