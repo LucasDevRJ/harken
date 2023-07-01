4033630c1d227aab94152919ec68f77ec2863a08
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-01 10:45:36 -0300

    encapsulando a Estacao

--- src/com/github/lucasdevrj/harken/modelos/Estacao.java
+++ src/com/github/lucasdevrj/harken/modelos/Estacao.java
@@ -1,4 +1,13 @@
 package com.github.lucasdevrj.harken.modelos;
 
 public class Estacao {
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
