fbcbe09032f5a22bb4f4a5378c730cb1eab015dd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-01 10:57:38 -0300

    método para reproduzir estação e muda-la

--- src/com/github/lucasdevrj/harken/modelos/Estacao.java
+++ src/com/github/lucasdevrj/harken/modelos/Estacao.java
@@ -2,6 +2,9 @@ package com.github.lucasdevrj.harken.modelos;
 
 public class Estacao {
     private String titulo;
+    private int duracaoTotalEscutada = 0;
+
+    private int quantidadeOuvintes = 0;
 
     public String getTitulo() {
         return titulo;
@@ -9,5 +12,28 @@ public class Estacao {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public int getDuracaoTotalEscutada() {
+        return duracaoTotalEscutada;
+    }
+
+    public void escutarEstacao(int minutos) {
+        this.duracaoTotalEscutada += minutos;
+    }
+
+    public void escutarEstacao() {
+        System.out.println("Você está escutando " + this.titulo);
+        this.quantidadeOuvintes++;
+    }
+
+    public void mudaEstacao(Estacao estacao) {
+        String estacaoDesejada = estacao.titulo;
+        if (!this.titulo.equals(estacaoDesejada)) {
+            System.out.println("Trocou da estação " + this.titulo + " para " + estacaoDesejada);
+            this.setTitulo(estacaoDesejada);
+        } else {
+            System.out.println("Já está na estação desejada!!");
+        }
     }
 }
--- src/com/github/lucasdevrj/harken/modelos/Radio.java
+++ src/com/github/lucasdevrj/harken/modelos/Radio.java
@@ -6,25 +6,21 @@ import java.util.ArrayList;
 
 public class Radio extends Audio {
 
-    private ArrayList<Radio> listaRadios = new ArrayList<Radio>();
+    private ArrayList<Estacao> listaEstacoes = new ArrayList<Estacao>();
     private int duracaoTotal = 0;
 
-    public ArrayList<Radio> getListaRadios() {
-        return listaRadios;
+    public void adicionaEstacao(Estacao estacao) {
+        System.out.println("A estação " + estacao.getTitulo() + " foi adicionada!!");
+        this.listaEstacoes.add(estacao);
     }
 
-    public void adicionaRadio(Radio radio) {
-        System.out.println("A estação " + radio.titulo + " foi adicionada!!");
-        this.listaRadios.add(radio);
-    }
-
-    public void exibeRadioMaisTocada() {
-        int radioMaisTocada = this.listaRadios.get(0).duracaoTotal;
+    public void exibeEstacaoMaisTocada() {
+        int estacaoMaisTocada = this.listaEstacoes.get(0).duracaoTotal;
         String nomeRadioMaisTocada = this.listaRadios.get(0).titulo;
 
-        for (int i = 0; i < this.listaRadios.size(); i++) {
-            if (this.listaRadios.get(i).duracaoTotal > radioMaisTocada) {
-                radioMaisTocada = this.listaRadios.get(i).duracaoTotal;
+        for (int i = 0; i < this.listaEstacoes.size(); i++) {
+            if (this.listaEstacoes.get(i).duracaoTotal > estacaoMaisTocada) {
+                estacaoMaisTocada = this.listaRadios.get(i).duracaoTotal;
                 nomeRadioMaisTocada = this.listaRadios.get(i).titulo;
             }
         }
@@ -32,21 +28,10 @@ public class Radio extends Audio {
         System.out.println("Radio mais tocada: " + nomeRadioMaisTocada);
     }
 
-    public void escutarRadioPorXMinutos(int minutos) {
+    public void escutarEstacaoPorXMinutos(int minutos) {
         this.setDuracao(minutos);
         this.duracaoTotal += minutos;
         this.totalDeReproducoes++;
         System.out.println("Você escutou a radio " + this.titulo + " por " + minutos + " minutos.");
-    }
-
-    public void mudaEstacao(Radio radio) {
-        String radioDesejada = radio.titulo;
-        if (!this.titulo.equals(radioDesejada)) {
-            System.out.println("Trocou da estação " + this.titulo + " para " + radioDesejada);
-            this.setTitulo(radioDesejada);
-            this.totalDeReproducoes++;
-        } else {
-            System.out.println("Já está na estação desejada!!");
-        }
     }
 }
