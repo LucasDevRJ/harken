2a4d184d10bb60f599a641952fa37030cec1be02
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 08:57:30 -0300

    atributos da classe Episodio junto com o encapsulamento

--- src/com/github/lucasdevrj/harken/modelos/Episodio.java
+++ src/com/github/lucasdevrj/harken/modelos/Episodio.java
@@ -1,4 +1,40 @@
 package com.github.lucasdevrj.harken.modelos;
 
 public class Episodio {
+    private String titulo;
+    private String data;
+    private int duracao;
+    private String descricao;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
