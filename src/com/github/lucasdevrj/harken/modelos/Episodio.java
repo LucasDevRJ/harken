30fd0f554c14a83ebe69d2fb8a669a4357a63956
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-07-05 09:09:43 -0300

    classe Episodio com os atributos e métodos

--- src/com/github/lucasdevrj/harken/modelos/Episodio.java
+++ src/com/github/lucasdevrj/harken/modelos/Episodio.java
@@ -5,36 +5,28 @@ public class Episodio {
     private String data;
     private int duracao;
     private String descricao;
+    private Podcast podcast;
 
-    public Episodio(String titulo, String data, int duracao, String descricao) {
+    public Episodio(String titulo, String data, int duracao, Podcast podcast) {
         this.titulo = titulo;
         this.data = data;
         this.duracao = duracao;
-        this.descricao = descricao;
+        this.podcast = podcast;
     }
-
     public String getTitulo() {
         return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
     }
 
     public String getData() {
         return data;
     }
 
-    public void setData(String data) {
-        this.data = data;
-    }
-
     public int getDuracao() {
         return duracao;
     }
 
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
+    public Podcast getPodcast() {
+        return podcast;
     }
 
     public String getDescricao() {
